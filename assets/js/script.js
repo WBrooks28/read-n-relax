c0dc036580854dd6e595e84e75d2abe02dc5a34e
Jared Long <longja2015@gmail.com>
2022-01-12 19:15:27 -0500

    deleted extra submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var submit = $('cityInput');
 //Code for map apperance
 let map = L.map('map', {
-    layers: MediaQueryList.mapLayer(),
+    layers: MQ.mapLayer(),
     center: [41.850033, -87.6500523],
     zoom: 4
 });

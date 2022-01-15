6a1465580408e2f25704aa6fe7e05a63bdecee63
Jared Long <longja2015@gmail.com>
2022-01-15 15:50:10 -0500

    added google maps, checkbox functions, and map styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
+const key = "AIzaSyBQyjNgRCUdgkuyAXYjvyBKMU3A2FKabeo";
+
 const quote = document.querySelector ("#quotes");
 const author = document.querySelector("#author");
+
+let checkboxValue = "";
 
 function getQuote(){
     console.log("Button clicked");
@@ -14,13 +18,69 @@ function getQuote(){
 
 getQuote();  
 
-var submit = $('cityInput');
-//Code for map apperance
-let map = L.map('map', {
-    layers: MQ.mapLayer(),
-    center: [39.830864, -98.580866],
-    zoom: 4
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 34.397, lng: 150.644 },
+        scrollwheel: false,
+        zoom: 4
+    });
+}
+
+function getCity() {
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${checkboxValue}+richmond+virginia&key=${key}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+
+function getAll() {
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=libaries+richmond+virginia&key=${key}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=bookstores+richmond+virginia&key=${key}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=parks+richmond+virginia&key=${key}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffee+richmond+virginia&key=${key}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+
+$(document).ready(function() {
+    $('#all').change(function () {
+        checkboxValue = $('#all').val(); 
+        console.log(checkboxValue);
+        getAll();
+     });
+     $('#libraries').change(function () {
+        checkboxValue = $('#libraries').val(); 
+        console.log(checkboxValue);
+        getCity();
+     });
+     $('#bookstores').change(function () {
+        checkboxValue = $('#bookstores').val(); 
+        console.log(checkboxValue);
+        getCity();
+     });
+     $('#coffee').change(function () {
+        checkboxValue = $('#coffee').val(); 
+        console.log(checkboxValue);
+        getCity();
+     });
+     $('#parks').change(function () {
+        checkboxValue = $('#parks').val(); 
+        console.log(checkboxValue);
+        getCity();
+     });
 });
-
-
-
